f3b33e49980dcaf42e4718c959956224e0520682
adhyayan-ai <singh.adh71@gmail.com>
2024-07-13 13:47:20 -0700

    Beta Version

--- ai_model/embedder.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-
-def get_embeddings(text):
-    sentences = text.split('\n')
-    embeddings = model.encode(sentences)
-    return sentences, embeddings
